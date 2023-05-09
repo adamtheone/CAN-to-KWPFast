faea882e14db306f7024b00ff84726dd76bea166
Adam Varga <dipcanmod@gmail.com>
2023-05-09 21:26:50 +0200

    seems to be working

--- can_to_kwp/can_to_kwp.ino
+++ can_to_kwp/can_to_kwp.ino
@@ -4,36 +4,39 @@
 // Dependencies:
 // - 'CAN' Arduino library (CANReceiverCallback.ino) - Sandeep Mistry
 // - 'OBD9141' Arduino library (readDTC.ino)
-// Includes
+// Includes -------------------------------------------------------------------
 #include "CAN.h"
 #include "OBD9141.h"
-// Defines
+// Defines --------------------------------------------------------------------
 #define KWP_RX_PIN 0
 #define KWP_TX_PIN 1
 #define KWP_EN_PIN 21
 
 #define CAN_SPEED 500E3
-#define DEBUG_PRINT 0
-// Global variables
+#define MCP2515_CS_PIN 10
+#define MCP2515_IRQ_PIN 2
+// Global variables -----------------------------------------------------------
 OBD9141 kwp;
 bool kwpInitSuccess = false;
-// Functions
-//  - Generic
-void (*resetFunc)(void) = 0;
-//  - KWP
+uint8_t kwpRetries = 0;
+
+// Functions ------------------------------------------------------------------
+//  KWP
+void (*resetFunc)(void) = 0; // for soft-restart
+
 uint8_t KWPRequest(uint8_t pid) {
-  uint8_t message[5] = { 0x68, 0x6A, 0xF1, 0x01, pid };
+  uint8_t message[5] = { 0x68, 0x6A, 0xF1, 0x01, pid }; // KWPFast query bytes
   return kwp.request(&message, 5);
 }
-//  - CAN
+//  CAN
 int getCANRequestedPid(void) {
   if (CAN.packetExtended() || CAN.packetId() != 0x7DF) {
     if (CAN.packetExtended()) {
-      Serial.print("Extended packet with id 0x");
+      Serial.print("Unexpected extended packet with id 0x");
     } else {
-      Serial.print("Normal packet with id 0x");
+      Serial.print("Unexpected normal packet with id 0x");
     }
-    Serial.print(CAN.packetId(), HEX);
+    Serial.println(CAN.packetId(), HEX);
 
     return -1;  // don't want to support extended packets for now
   }
@@ -43,14 +46,14 @@ int getCANRequestedPid(void) {
 
   while (CAN.available()) {
     payload[packetLen++] = CAN.read();
-    //Serial.print(payload[packetPtr-1], HEX);
-    //Serial.print(" ");
   }
 
   return packetLen > 2 ? payload[2] : -1;
 }
 
 void sendCanRespDummy(uint8_t pid) {
+  Serial.print("Sending dummy: ");
+  Serial.println(pid, HEX);
   CAN.beginPacket(0x7E8, 8);
   CAN.write(0x07);  // number of additional bytes
   uint8_t padding = 8;
@@ -151,6 +154,7 @@ void sendCanRespDummy(uint8_t pid) {
   }
   CAN.endPacket();
 }
+
 void onCanReceive(int packetSize) {
   if (!kwpInitSuccess) {
     int pid = getCANRequestedPid();
@@ -162,21 +166,24 @@ void onCanReceive(int packetSize) {
 
 void sendCanResp(uint8_t pid, uint8_t dataLen) {
   CAN.beginPacket(0x7E8, 8);
-  CAN.write(dataLen);
+  CAN.write(dataLen+2);
   CAN.write(0x41);
   CAN.write(pid);
+  Serial.print("Useful data: ");
   for (uint8_t i = 0; i < 5; i++) {
     if (i <= dataLen) {
-      CAN.write(kwp.readBuffer(i));
+      CAN.write(kwp.readUint8(i));
+      Serial.print(kwp.readUint8(i), HEX);
+      Serial.print(" ");
     } else {
       CAN.write(0x00);
     }
   }
+  Serial.println();
   CAN.endPacket();
 }
 
-
-// Setup
+// Setup ----------------------------------------------------------------------
 void setup() {
   // Serial setup
   Serial.begin(115200);
@@ -185,13 +192,19 @@ void setup() {
   Serial.println("CAN to KWPFast started!");
 
   // CAN setup
-  CAN.setPins(10, 2);
+  CAN.setPins(MCP2515_CS_PIN, MCP2515_IRQ_PIN);
   if (!CAN.begin(500E3)) {
     Serial.println("Starting CAN failed!");
     delay(1500);
     resetFunc();
   }
-  CAN.onReceive(onCanReceive);
+  //-----------------------------------------------------
+  // We specifically want to answer to the CAN master
+  //  only if KWP is initialized successfully.
+  //  Uncommenting the following line activates the dummy
+  //  responses even before KWP is initialized.
+  //-----------------------------------------------------
+  // CAN.onReceive(onCanReceive);
 
   // KWPFast setup
   pinMode(KWP_EN_PIN, OUTPUT);
@@ -205,6 +218,8 @@ void loop() {
     kwpInitSuccess = kwp.initKWP();
     if (kwpInitSuccess) {
       Serial.println("KWP Init successful!");
+      CAN.onReceive(NULL);
+      kwpRetries = 0;
     } else {
       Serial.println("KWP Init unsuccessful!");
     }
@@ -212,14 +227,18 @@ void loop() {
     int packetSize = CAN.parsePacket();
     if (packetSize) {
       int pid = getCANRequestedPid();
-      if (pid) {
+      if (pid > 0) {
         Serial.print("can pid: 0x"); Serial.println(pid, HEX);
         uint8_t rxLen = KWPRequest((uint8_t)pid);
         if (rxLen) {
           sendCanResp(pid, rxLen-5);
         } else {
-          kwpInitSuccess = false;
-          sendCanRespDummy(pid);
+          Serial.println("No KWP response!");
+          kwpRetries++;
+          if (kwpRetries > 5) {
+            kwpInitSuccess = false;
+          }
+          //sendCanRespDummy(pid);
         }
       }
     }
